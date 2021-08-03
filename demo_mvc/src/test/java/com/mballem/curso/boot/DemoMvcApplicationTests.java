9c6bba376f8ae243f1fd71e12042a16e16b70db4
Vitória Gonçalves <vitoriagcf@hotmail.com>
2021-08-03 00:16:45 -0300

    Tests

--- demo_mvc/src/test/java/com/mballem/curso/boot/DemoMvcApplicationTests.java
+++ demo_mvc/src/test/java/com/mballem/curso/boot/DemoMvcApplicationTests.java
@@ -2,7 +2,7 @@ package com.mballem.curso.boot;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@RunWith(SpringRunner.class)
+////@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoMvcApplicationTests {
 
